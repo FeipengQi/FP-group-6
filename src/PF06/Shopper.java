5f22f55bba5ed8c78f4ade7d7bfdc27be1b49d57
FeipengQi <qfpeng99@brandeis.com>
2020-04-28 23:37:02 -0400

    comment on shopper and customer..

--- src/PF06/Shopper.java
+++ src/PF06/Shopper.java
@@ -1,14 +1,19 @@
 package PF06;
 
+/*
+ * Shopper class represents the different feature of a given shopper. 
+ */
 public class Shopper {
 	private GroceryStore store;
     private int startTime;
     private int endTime;
     private boolean occupied;
-    private boolean delivered;
     private int timeLeft;
     private Customer customer;
 
+    /*
+     * constructor for shopper
+     */
     public Shopper(GroceryStore store, int startTime, int endTime) {
       this.store = store;
       this.startTime = startTime;
@@ -17,48 +22,60 @@ public class Shopper {
       this.occupied = false;
       this.delivered = false;
     }
-
+    /*
+     * shopper's earliest available time for delivering..
+     */
     public int getStartTime(){
       return this.startTime;
     }
-
+    /*
+     * shopper's latest available time for delivering..
+     */
     public int getEndTime(){
       return this.endTime;
     }
-
+    /*
+     * shopper's status(if he is in a delivery process)
+     */
     public boolean isOccupied(){
       return this.occupied;
     }
-
+    /*
+     * change shopper's status when starting the delivery..
+     */
     public void startDelivery(Customer customer) {
       this.occupied = true;
       this.customer = customer;
     }
-
+    /*
+     * time left for delivery..
+     */
     public int getTimeLeft(){
         return this.timeLeft;
     }
-    public boolean isDelivered(){
-        if (this.timeLeft >= 0){
-            return false;
-        } else {
-            return true;
-        }
-    }
+    /*
+     * customer's products have been delivered..
+     */
     public void delivered(){
         this.occupied = false;
         this.timeLeft = 1;
     }
 
-
+    /*
+     * which customer the shopper is serving.. 
+     */
     public Customer getCustomer() {
         return this.customer;
     }
-
+    /*
+     * the shopper is on the way and will take 2 hours for delivering.. 
+     */
     public void approaching(){
         this.timeLeft--;
     }
-
+    /*
+     * which store does the shopper represent.. 
+     */
     public GroceryStore getGroceryStore() {
         return this.store;
     }
